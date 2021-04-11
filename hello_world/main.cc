373099a3f5eff781c724f214e1a5d21d5fc47616
Tim Maloney <tmaloney@pdx.edu>
2021-04-11 12:48:22 -0700

    Update main.cc
    Cleaned it up a bit.

--- hello_world/main.cc
+++ hello_world/main.cc
@@ -12,20 +12,7 @@ color ray_color(const ray &r) {
 
 int main() {
 
-   /* // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-
-    // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);*/
+   // Image
     const int image_width = 256;
     const int image_height = 256;
 
@@ -35,15 +22,6 @@ int main() {
     for(int i = image_height - 1; i >= 0; --i){
         std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
         for(int j = 0; j < image_width; ++j){
-            /*
-            auto v = double(i) / (image_width - 1);
-	    auto u = double(j) / (image_height - 1);
-	    
-	    ray r(origin, lower_left_corner + v*horizontal + u*vertical - origin);
-
-	    color pixel_color = ray_color(r);
-	    write_color(std::cout, pixel_color);*/
-
 	    color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
 	    write_color(std::cout, pixel_color);
 	}
